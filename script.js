d325713424c2b6dc1ec38d1d219e19d7bb836e7b
lauravcruz <lauravcruz@hotmail.com>
2022-06-08 20:20:20 +0200

    a

--- script.js
+++ script.js
@@ -295,3 +295,13 @@ function comprobar(elemento, hecho){
 
     return booleano; 
 }
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
